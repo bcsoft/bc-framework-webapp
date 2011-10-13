cc69a4867f08424a6d44306be3765bf49c1fafa6
rongjihuang <rongjihuang@gmail.com>
2011-10-13 16:05:58 +0800

    当对话框的appendTo为非body时遮罩层将盖住对话框的修正

--- libs/dialog.js
+++ libs/dialog.js
@@ -185,4 +185,54 @@ $.extend($.ui.dialog.prototype, {
 	}
 });
 
+//遮罩的扩展
+$.extend( $.ui.dialog.overlay, {
+	create: function( dialog ) {
+		if ( this.instances.length === 0 ) {
+			// prevent use of anchors and inputs
+			// we use a setTimeout in case the overlay is created from an
+			// event that we're going to be cancelling (see #2804)
+			setTimeout(function() {
+				// handle $(el).dialog().dialog('close') (see #4065)
+				if ( $.ui.dialog.overlay.instances.length ) {
+					$( document ).bind( $.ui.dialog.overlay.events, function( event ) {
+						// stop events if the z-index of the target is < the z-index of the overlay
+						// we cannot return true when we don't want to cancel the event (#3523)
+						if ( $( event.target ).zIndex() < $.ui.dialog.overlay.maxZ ) {
+							return false;
+						}
+					});
+				}
+			}, 1 );
+
+			// allow closing by pressing the escape key
+			$( document ).bind( "keydown.dialog-overlay", function( event ) {
+				if ( dialog.options.closeOnEscape && !event.isDefaultPrevented() && event.keyCode &&
+					event.keyCode === $.ui.keyCode.ESCAPE ) {
+					
+					dialog.close( event );
+					event.preventDefault();
+				}
+			});
+
+			// handle window resize
+			$( window ).bind( "resize.dialog-overlay", $.ui.dialog.overlay.resize );
+		}
+
+		var $el = ( this.oldInstances.pop() || $( "<div>" ).addClass( "ui-widget-overlay" ) )
+			.appendTo( dialog.options.appendTo || document.body )//修改的代码
+			.css({
+				width: this.width(),
+				height: this.height()
+			});
+
+		if ( $.fn.bgiframe ) {
+			$el.bgiframe();
+		}
+
+		this.instances.push( $el );
+		return $el;
+	}
+});
+
 }(jQuery));
