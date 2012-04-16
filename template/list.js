4bab4d7ace3e45a36d8c627022096ca6ad12e2d7
fatfish <fatfish2010@sina.cn>
2012-04-16 09:17:59 +0800

    模板管理视图修改下载和在线预览逻辑

--- template/list.js
+++ template/list.js
@@ -1,29 +1,43 @@
 bc.templateList = {
 	/** 在线预览 */
 	inline: function(){
-		var $tds = $(this).find(".bc-grid>.data>.left tr.ui-state-highlight>td.id");
+		var $view=$(this);
+		var $tds = $view.find(".bc-grid>.data>.left tr.ui-state-highlight>td.id");
 		if($tds.length == 1){
-			//在新窗口中打开文件
-			window.open(bc.root + "/bc/template/inline?to=pdf&id=" + $tds.attr("data-id"), "_blank");
+			//取类型列data-value的值
+			var type= $view.find(".bc-grid>.data>.right tr.ui-state-highlight>td:eq(2)").attr("data-value");
+			if(type==3||type==4){
+				bc.msg.slide("文本文件或html文件，请直接双击表格查看！");
+			}else{
+				//在新窗口中打开文件
+				window.open(bc.root + "/bc/template/inline?to=pdf&id=" + $tds.attr("data-id"), "_blank");
+			}
 		}else if($tds.length > 1){
-			bc.msg.slide("一次只可以预览一个附件，请确认您只选择了一个附件！");
+			bc.msg.slide("一次只可以预览一个模板，请确认您只选择了一个模板！");
 			return;
 		}else{
-			bc.msg.slide("请先选择要预览的附件！");
+			bc.msg.slide("请先选择要预览的模板！");
 			return;
 		}
 
 	},
 	/** 下载选择的*/
 	download: function(){
+		var $view=$(this);
 		var $tds = $(this).find(".bc-grid>.data>.left tr.ui-state-highlight>td.id");
 		if($tds.length == 1){
-			window.open(bc.root + "/bc/template/download?id=" + $tds.attr("data-id"), "blank");
+			//取类型列data-value的值
+			var type= $view.find(".bc-grid>.data>.right tr.ui-state-highlight>td:eq(2)").attr("data-value");
+			if(type==3||type==4){
+				bc.msg.slide("文本文件或html文件，请直接双击表格查看！");
+			}else{
+				window.open(bc.root + "/bc/template/download?id=" + $tds.attr("data-id"), "blank");
+			}
 		}else if($tds.length > 1){
-			bc.msg.slide("一次只可以下载一个附件，请确认您只选择了一个附件！");
+			bc.msg.slide("一次只可以下载一个模板，请确认您只选择了一个模板！");
 			return;
 		}else{
-			bc.msg.slide("请先选择要下载的附件！");
+			bc.msg.slide("请先选择要下载的模板！");
 			return;
 		}
 	},
