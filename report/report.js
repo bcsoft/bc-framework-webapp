adf639e2e93bd932985893d3bad1b7ad7ca4b567
dragon <rongjihuang@gmail.com>
2012-06-06 18:53:00 +0800

    修正报表错误

--- report/report.js
+++ report/report.js
@@ -7,7 +7,7 @@ bc.report = {
 		
 		bc.msg.confirm("确定要将当前报表结果存为历史报表吗？",function(){
 			// 显示提示信息
-			var win = bc.msg.info("<div style='font-size:20px;'>正在存为历史报表...</div><div>报表执行可能比较耗时，请耐心等候!<div>",null);
+			var win = bc.msg.info("<div id='msg' style='font-size:20px;'>正在存为历史报表...</div><div id='info'>报表执行可能比较耗时，请耐心等候!</div>",null);
 			
 			// 获取条件信息
 			var data = {};
@@ -19,14 +19,26 @@ bc.report = {
 			bc.ajax({
 				url: bc.root + "/bc/report/save2history",
 				data: data,
+				dataType:"json",
 				success: function(json){
-					if(win){
-						try{
-							win.dialog("destroy").remove();
-						}catch(e){}
+					logger.info("success=" + json.success);
+					if(json.success){
+						if(win){
+							try{
+								win.dialog("destroy").remove();
+							}catch(e){}
+						}
+						bc.msg.slide("存为历史报表成功！");
+					}else{
+						if(win){
+							win.find("#msg").html("存为历史报表失败了！");
+							logger.info(json.msg);
+							logger.info(win.find("#info").size());
+							win.find("#info").html(json.msg);
+						}else{
+							bc.msg.alert("存为历史报表失败了：" + json.msg);
+						}
 					}
-					bc.msg.slide("存为历史报表成功！");
-					logger.info("success");
 				}
 			});
 		});
