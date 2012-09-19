92a24655acdc4cfc492ccd4f6cdefed04fabcfba
dragon <rongjihuang@gmail.com>
2012-09-19 15:45:50 +0800

    重新合并js、css

--- libs/bc.js
+++ libs/bc.js
@@ -417,19 +417,19 @@ bc.msg = {
     },
     /** 信息警告框：提示框icon=warning的简化使用版 */
     warn: function(msg, title, onOk){
-    	alert("TODO");
+    	alert("TODO:warn");
     },
     /** 错误提示框：提示框icon=error的简化使用版 */
     error: function(msg, title, onOk){
-    	alert("TODO");
+    	alert("TODO:error");
     },
     /** 信息提问框：提示框icon=question的简化使用版 */
     question: function(msg, title, onOk){
-    	alert("TODO");
+    	alert("TODO:question");
     },
     /** 自动提醒框：显示在页面右下角并可以自动隐藏的消息提示框 */
     show: function(config){
-    	alert("TODO");
+    	alert("TODO:show");
     },
     /** 自动提醒框的slide简化使用版:滑出滑入效果 */
     slide: function(msg,timeout,width,height){
@@ -2948,6 +2948,12 @@ $("ul li.pagerIcon").live("click", function() {
 		else
 			alert("'bc.grid.export2Excel'未定义");
 		break;
+	case "import"://导入数据
+		if(bc.grid.import)
+			bc.grid.import($page.find(".bc-grid"),this);
+		else
+			alert("'bc.grid.import'未定义");
+		break;
 	default ://调用自定义的函数
 		var click = $this.attr("data-click");
 		if(typeof click == "string")
@@ -3353,6 +3359,109 @@ bc.grid.export2Excel = function($grid,el) {
 		});
 
 		return false;
+	});
+};
+
+})(jQuery);
+/**
+ * 列表视图插件：从Excel导入数据
+ * 
+ * @author rongjihuang@gmail.com
+ * @date 2012-09-18
+ * @depend grid.js
+ */
+(function($) {
+
+/**
+ * 显示导出视图数据的配置界面-->用户选择-->导出excel
+ * @param $grid 表格的jquery对象
+ * @param el 导出按钮对应的dom元素
+ */
+bc.grid.import = function($grid,el) {
+	var cfg = $(el).data("cfg") || {};// 配置信息
+	var html=[];
+	html.push('<form class="bc-import" name="importer" method="post">');
+	html.push('<div class="info" style="width:14em;font-weight:normal;padding:5px;border:1px dashed #ccc;">导入数据前请确保数据文件符合模板格式，可以通过下载模板获知格式的详细要求！</div>'
+		+'<div class="buttons" style="text-align: right;padding: 0 4px;">');
+	
+	// 上传数据按钮
+	html.push('<a id="upload" style="text-decoration:underline;cursor:pointer;position: relative;">上传数据'
+		+'<input type="file" class="uploadFile" id="uploadFile" name="uploadFile" title="点击上传文件"'
+		+' data-cfg=\'{"subdir":"import","ptype":"import.option","puid":"import.option"}\''
+		+' style="position: absolute;left: 0;top: 0;width: 100%;height: 100%;filter: alpha(opacity = 10);opacity: 0;cursor: pointer;"/>'
+		+'</a>&nbsp;&nbsp;');
+	
+	// 下载模板、取消 按钮
+	html.push('<a id="download" style="text-decoration:underline;cursor:pointer;">下载模板</a>&nbsp;&nbsp;'
+		+'<a id="cancel" style="text-decoration:underline;cursor:pointer;">取消</a></div>'
+		+'</form>');
+	html = html.join("");
+	
+	//显示窗口
+	var boxPointer = bc.boxPointer.show({
+		of:el,dir:"top",close:"click",
+		offset:"-8 -4",
+		iconClass:null,
+		appendTo: $grid.closest(".ui-dialog"),
+		content:html
+	});
+	
+	//取消按钮
+	boxPointer.find("#cancel").click(function(){
+		boxPointer.remove();
+		return false;
+	});
+	
+	//下载按钮
+	boxPointer.find("#download").click(function(){
+		if(!cfg.tplCode){
+			alert("没有定义所要下载模板的编码值，无法下载！");
+			return;
+		}
+		
+		// 下载模板
+		var url = bc.root + "/bc/templatefile/download?code=" + cfg.tplCode;
+		window.open(url, "blank");
+		
+		return false;
+	});
+	
+	// 上传按钮
+	var $file = boxPointer.find("#uploadFile");
+	var fileOption = $file.data("cfg");
+	// 上传文件完成后的回调函数,上下文为file控件，参数为文件上传后服务器返回的json信息
+	fileOption.callback = function(json){
+		//alert("import_upload_callback: " + json.file);
+		if(!cfg.importAction){
+			alert("没有定义导入数据的Action");
+			return;
+		}
+		
+		// 调用action导入数据
+		var data =  {file: json.file};
+		if(cfg.headerRowIndex) data.headerRowIndex = cfg.headerRowIndex;
+		$.ajax({
+			dataType: "json",
+			data: data,
+			url: bc.root + "/" + cfg.importAction,
+			success: function(json){
+				if(json.success){// 成功
+					bc.msg.info(json.msg, null, function(){
+						// 关闭导入窗口
+						boxPointer.remove();
+						
+						// 刷新视图
+						bc.grid.reloadData($grid.closest(".bc-page"));
+					});
+				}else{// 失败
+					bc.msg.info(json.msg);
+				}
+			}
+		});
+	};
+	$file.change(function(e){
+		logger.info("localfile=" + this.value);
+		bc.file.upload.call(this,e.target.files,fileOption);
 	});
 };
 
