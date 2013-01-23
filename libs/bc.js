d0ae1e36b9f87c1d0e10278bc59adb373bceb63e
dragon <rongjihuang@gmail.com>
2013-01-23 12:11:59 +0800

    重新合并js

--- libs/bc.js
+++ libs/bc.js
@@ -215,6 +215,19 @@ bc.getTime = function(){
 		time += now.getSeconds();
 	return time;
 };
+/** 格式化时间信息，格式为HH:mm:ss
+ * @return 
+ */
+bc.formatTime = function(hour,minute,second,millisecond){
+	var time = (hour < 10 ? "0" : "") + hour;//时
+	if(typeof minute != "undefined")
+		time += (minute < 10 ? ":0" : ":") + minute;//分
+	if(typeof second != "undefined")
+		time += (second < 10 ? ":0" : ":") + second;//秒
+	if(typeof millisecond != "undefined")
+		time += millisecond;//豪秒
+	return time; 
+};
 /**
  * 计算指定时间范围内的耗时描述信息
  * 
@@ -3527,24 +3540,124 @@ bc.grid.import = function($grid,el) {
 			return;
 		}
 		
+		var $page = $(this).closest(".ui-dialog").children(".bc-page");
+		var title = $page.dialog("option", "title")
+		
+		// 显示导入进度对话框
+		var doing = true;
+		var $processDlg = '<div data-type="custom" class="bc-page">';
+		$processDlg += '<div class="info">正在导入数据，请耐心等候！</div>';
+		$processDlg += '<div class="time" style="text-align:center;font-size:200%">00:00:00</div>';
+		$processDlg += '</div>';
+		$processDlg = $($processDlg);
+		$processDlg.dialog({title:"导入" + title, modal: true});
+		
+		// 定时更新耗时值
+		var time = new Date(1976,0,1,0,0,0);
+		var timeEl = $processDlg.children(".time").get(0);
+		var timeId = window.setInterval(function(){
+			time.setSeconds(time.getSeconds()+1);
+			timeEl.innerText = bc.formatTime(time.getHours(),time.getMinutes(),time.getSeconds());
+		}, 1000);
+		$processDlg.bind("dialogclose",function(event,ui){
+			// 删除定时器
+			window.clearInterval(timeId);
+
+			// 关闭导入窗口
+			boxPointer.remove();
+			
+			// 刷新视图
+			if($processDlg.data("refresh"))
+				bc.grid.reloadData($grid.closest(".bc-page"));
+			
+			// 彻底删除所有相关的dom元素
+			$processDlg.dialog("destroy").remove();
+		}).bind("dialogbeforeclose",function(event,ui){
+			if(doing) return false;// 请求未完成不允许关闭窗口
+		});
+
 		// 调用action导入数据
-		var data =  {file: json.file};
+		var data = {file: json.file};
 		if(cfg.headerRowIndex) data.headerRowIndex = cfg.headerRowIndex;
 		$.ajax({
 			dataType: "json",
 			data: data,
 			url: bc.root + "/" + cfg.importAction,
+			complete: function(){
+				doing = false;
+				window.clearInterval(timeId);// 删除定时器
+			},
 			success: function(json){
-				if(json.success){// 成功
-					bc.msg.info(json.msg, null, function(){
-						// 关闭导入窗口
-						boxPointer.remove();
+				var msg = json.msg;
+				var detailID = "detail-"+new Date().getTime();
+				if(json.detail){
+					msg += '&nbsp;<a href="#" id="'+detailID+'">点击查看详情</a>';
+				}
+				$processDlg.children(".info").html(msg);
+				if(json.success) $processDlg.data("refresh",true);
+				if(json.detail){
+					$processDlg.find("#"+detailID).click(function(){
+						//打开查看详情的窗口
+						var errorWin=window.open('', 'showImportDetail');
+						var errorDoc = errorWin.document;
+						errorDoc.open();
+						var html = [];
+						html.push('<!DOCTYPE html>');
+						html.push('<html>');
+						html.push('<head>');
+						html.push('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>');
+						// 获取视图的标题
+						html.push('<title>'+title+' - 数据导入异常列表</title>');
+						html.push('<style type="text/css">');
+						html.push('body>div{color:red;font-weight:bold;font-size:150%}');
+						html.push('table{border:none;border-collapse:collapse}');
+						html.push('thead{font-weight:bold;}');
+						html.push('td{border:1px solid gray;padding:2px;}');
+						html.push('td:nth-child(1),td:nth-child(2){color:red;font-weight:bold;}');
+						html.push('td:nth-child(2){max-width:600px;}');
+						html.push('</style>');
+						html.push('</head>');
+						html.push('<body>');
 						
-						// 刷新视图
-						bc.grid.reloadData($grid.closest(".bc-page"));
+						html.push('<div>'+title+' - 数据导入异常列表</div>');
+						html.push('<table>');
+						
+						// 获取列名
+						var d = json.detail;
+						var columnNames = json.columnNames;
+						//alert(columnNames);
+						
+						// 构建列头行
+						html.push('<thead><tr>');
+						html.push('<td>行号</td><td>异常信息</td>');
+						for(var i=0;i<columnNames.length;i++){
+							html.push('<td>'+columnNames[i]+'</td>');
+						}
+						html.push('</tr></thead>');
+						
+						// 构建数据行
+						html.push('<tbody>');
+						var hri = cfg.headerRowIndex + 2;
+						for(var i=0;i<d.length;i++){
+							html.push('<tr>');
+							html.push('<td>'+(hri+parseInt(d[i].index))+'</td>');
+							html.push('<td>'+d[i].msg+'</td>');
+							var v;
+							for(var j=0;j<columnNames.length;j++){
+								v = d[i][columnNames[j]];
+								html.push('<td>'+(v?v:"")+'</td>');
+							}
+							html.push('</tr>');
+						}
+						html.push('</tbody></table>');
+						
+						html.push('</body>');
+						html.push('</html>');
+						errorDoc.write(html.join(""));
+						errorDoc.close();
+						errorWin.focus();
+						return false;
 					});
-				}else{// 失败
-					bc.msg.info(json.msg);
 				}
 			}
 		});
@@ -3554,6 +3667,11 @@ bc.grid.import = function($grid,el) {
 		bc.file.upload.call(this,e.target.files,fileOption);
 	});
 };
+
+/** 显示导入处理的详细信息 */
+function showImportDetail(){
+	
+}
 
 })(jQuery);
 /**
