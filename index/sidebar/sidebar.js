4801cfe350d43818206a5d8340a6c636173b8ba3
dragon <rongjihuang@gmail.com>
2013-03-26 19:37:37 +0800

    初步整合

--- index/sidebar/sidebar.js
+++ index/sidebar/sidebar.js
@@ -1,124 +1,209 @@
 bc.namespace("bc.sidebar");
-bc.sidebar = {
-	init : function() {
-		var $page = $(this);
-		var eventType = ($.support.touch ? "touchend" : "click");
-		// 绑定查看更多事件
-		$page.delegate(".more", eventType, function(){
-			bc.page.newWin({
-				name: "我的待办",
-				mid: "personals",
-				url: bc.root+ "/bc-workflow/todo/personals/list",
-			});
-			return false;
-		});
-		
-		// 绑定刷新事件
-		$page.delegate(".refresh", eventType, function(){
-			//bc.sidebar.refresh();
-			bc.sidebar.todo.refresh.call($page);
-			return false;
-		});
-		
-		// 自动刷新
-		$page.data("timer",setInterval(bc.sidebar.todo.refresh, 300000));
-		
-		// 绑定全局折叠事件
-		$page.delegate(".reverse", eventType, function(){
-			var $tasks = $(this).closest(".sidebar-todo").find("tr.task");
-			$tasks.toggleClass("collapse");
-			$tasks.find(".toggle").toggleClass("ui-icon-carat-1-se ui-icon-carat-1-nw");
-			return false;
-		});
-		
-		$page.delegate(".task", eventType, function(e) {
-			var $target = $(e.target);
-			var $this = $(this);
-			var taskId = $this.find(".taskidvalue").children().val();
-			var procInstId = $this.find(".procinstidvalue").children().val();
-			if($target.is(".toggle")){// 折叠或展开当前任务
-				$this.toggleClass("collapse");
-				$target.toggleClass("ui-icon-carat-1-se ui-icon-carat-1-nw");
-			}else if($target.is(".group")){// 领取任务
-				// 检测是否已有签领记录，没有才允许继续签领，避免重复签领
-				jQuery.ajax({
-					url: bc.root + "/bc-workflow/todo/personals/isSigned", 
-					data: {excludeId: taskId}, 
-					dataType: "json",
-					success: function(json) {
-						// 如果已经签领过就提示用户
-						if(json.signed == "true"){
-							bc.msg.alert('此任务已经签领.');
-						}else{
-							bc.msg.confirm("确定签领此任务吗？",function(){
-								jQuery.ajax({
-									url: bc.root + "/bc-workflow/todo/personals/claimTask", 
-									data: {excludeId: taskId},
-									dataType: "json",
-									success: function(json) {
-										bc.msg.slide(json.msg);
-										bc.sidebar.refresh();
-									}
-								});
-							});
-							return;
-						}
-					}
-				});
-			}else if($target.is(".name")){// 打开任务
-				bc.page.newWin({
-					name: "工作空间",
-					mid: "workspace"+procInstId,
-					url: bc.root+ "/bc-workflow/workspace/open?id="+procInstId
-				});
-			}
-			return false;
-		});
+(function($){
+
+// 通知：id,type,title,content,time
+var Notice = Backbone.Model.extend({
+	// 默认属性
+	defaults: function() {
+ 		return {
+	 		title: "(空)",
+	 		iconClass: "ui-icon-radio-on"
+		};
 	},
-	
-	/** 
-	 * 刷新待办边栏的数据
-	 * 
-	 */
-	refresh: function(){
-		var $sidebar = $(this);
-		if(!$sidebar.is(".sidebar-todo")){
-			$sidebar = $("#right").children(".sidebar-todo");
+
+	// 初始化
+	initialize: function() {
+ 		// 如果没有就添加默认的标题
+	 	if (!this.get("title")) {
+	 		this.set({"title": this.defaults().title});
 		}
-		
-		// 显示加载动画
-		var $loader = $sidebar.children('#sidebarLoader');
-		if($loader.size() == 0){
-			$loader = $('<img id="sidebarLoader" src="'+bc.root+'/bc/libs/themes/default/images/loader/loader02_64x64.gif" style="position:absolute;top:50%;margin-top:-32px;;left:50%;margin-left:-32px;"/>');
-			$loader.appendTo($sidebar);
+	 	if (!this.get("iconClass")) {
+	 		this.set({"iconClass": this.defaults().iconClass});
 		}
-		$loader.removeClass("hide");
-		
-		// 加载待办页面
-		bc.ajax({
-			url: bc.root + "/bc-workflow/todo/personals/sidebar",
-			dataType: "html",
-			success: function(html){
-				var $dom = $(html).children(".empty,.tasks");
-				$sidebar.children(".empty,.tasks").remove();
-				$sidebar.append($dom);
-				
-				// 删除加载动画
-				$loader.addClass("hide");
-			},
-			error: function(){
-				// error
-				bc.msg.info("加载待办信息异常！");
-				
-				var timer = $sidebar.data("timer");
-				if(timer){
-					window.clearInterval(timer);
-					jQuery.removeData($sidebar,"timer");
-				}
-				
-				// 删除加载动画
-				$loader.addClass("hide");
-			}
-		});
 	}
-};
+});
+
+// 通知列表
+var NoticeList = Backbone.Collection.extend({
+	// 获取数据的URl
+	url: "sidebar",
+
+	// 关联到通知
+	model: Notice
+});
+
+// 创建一个通知列表实例
+var notices = new NoticeList;
+
+// 通知条目的视图
+var NoticeView = Backbone.View.extend({
+	tagName: "div",
+	className: "row",
+
+	// Cache the template function for a single item.
+	template: _.template('<div class="header row">'
+		+'		<div class="icon ui-icon <%- iconClass %>"></div>'
+		+'		<div class="label"><%- title %></div>'
+		+'		<div class="time"><%- time4moment %></div>'
+		+'	</div>'
+		+'	<pre class="detail"><%- content %></pre>'),
+
+	// 绑定DOM事件
+	events: {
+		"click .row": "open"
+	},
+
+	// 初始化
+	initialize: function() {
+		// 监听模块的事件
+		this.listenTo(this.model, 'change', this.render);
+		this.listenTo(this.model, 'destroy', this.remove);
+	},
+
+	// 渲染
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	},
+
+	// 打开链接
+	open: function() {
+		alert("open:id=" + this.model.id);
+	}
+});
+
+//通知条目的视图
+var NoticeGroupView = Backbone.View.extend({
+	tagName: "div",
+	className: "group",
+	type: "none",
+	template: _.template(
+		'<div class="header row ui-widget-header">'
+			+'<div class="icon ui-icon <%- iconClass %>"></div>'
+			+'<div class="label"><span><%- title %></span></div>'
+			+'<div class="btn close ui-icon ui-icon-close"></div>'
+		+'</div>'
+		+'<div class="rows"></div>'),
+
+	// 绑定DOM事件
+	events: {
+		"click >.header>.label>span": "open",
+		"click >.header>.btn.close": "close"
+	},
+
+	// 初始化
+	initialize: function() {
+	},
+
+	// 渲染
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	},
+
+	// 打开更多视图
+	open: function() {
+		alert("TODO: group.type=" + this.type);
+	},
+
+	// 关闭分组
+	close: function() {
+		alert("TODO: group.close");
+	},
+
+	// 在组中添加一项
+	add: function(notice) {
+	    var view = new NoticeView({model: notice});
+	    this.$el.append(view.render().el);
+	}
+});
+
+// 待办边栏视图
+var SidebarView = Backbone.View.extend({
+	el: $("#right"),
+	groupViews: {},
+
+	// 界面模版
+	template: _.template(
+		 '<div class="header ui-widget-header">'
+			+'<div class="btns">'
+//				+'<div class="btn toggle">'
+//					+'<span class="ui-icon ui-icon-transferthick-e-w"></span>'
+//				+'</div>'
+				+'<div class="btn config">'
+					+'<span class="ui-icon ui-icon-wrench"></span>'
+				+'</div>'
+			+'</div>'
+			+'<div class="label">通知中心</div>'
+			+'<div class="search">'
+				+'<a href="#" class="ui-icon ui-icon-search" title="点击执行查询"></a>'
+				+'<input type="text" class="ui-widget-content" placeholder="搜索">'
+			+'</div>'
+		+'</div>'
+		+'<div class="groups ui-widget-content"></div>'),
+
+	// 定义事件委托
+	events: {
+		"click >.header>.btns>.btn.config": "config",
+		"keypress >.header>.search>input": "searchOnEnter"
+	},
+
+	// 初始化
+	initialize: function() {
+		// 监听模块列表的事件
+		this.listenTo(notices, 'add', this.addOne);
+		this.listenTo(notices, 'reset', this.addAll);
+		this.listenTo(notices, 'all', this.render);
+
+		// 加载数据
+		notices.fetch();
+	},
+
+ 	// 渲染
+ 	render: function() {
+		this.$el.toggleClass("sidebar",true).html(this.template({}));
+		
+		// 缓存一些内部的dom对象
+		this.input = this.$(">.header>.search>input");
+		this.groups = this.$(">.groups");
+
+		return this;
+	},
+
+ 	// 配置通知中心
+ 	config: function () {
+ 		alert("TODO: config");
+ 	},
+
+ 	// 回车后搜索
+	searchOnEnter: function(e) {
+ 	   	if (e.keyCode != 13) return;
+ 	   	if (!this.input.val()) return;
+ 	   	alert("TODO: searchOnEnter:" + this.input.val());
+ 	},
+
+	// 在组中添加一项
+	add: function(notice) {
+		if(!groupViews[notice.type]){
+			// 创建分组
+		    var view = new NoticeGroupView;
+		    this.$el.append(view.render().el);
+		    groupViews[notice.type] = view;
+		}
+		groupViews[notice.type].add(notice);
+	},
+
+	// 在组中重新添加全部
+	addAll: function() {
+		notices.each(this.addOne, this);
+	},
+
+	// 刷新
+	refresh: function() {
+		alert("TODO: sidebar.refresh");
+	}
+});
+
+// 创建边栏的实例
+bc.sidebar = new SidebarView;
+})(jQuery);
