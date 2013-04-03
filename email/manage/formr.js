84c684e80d982a5689b0f225a76b08e47d37919c
fatfish <fatfish2010@sina.cn>
2013-04-03 15:31:50 +0800

    邮件管理表单增加显示（时间前。。）的信息

--- email/manage/formr.js
+++ email/manage/formr.js
@@ -1,6 +1,9 @@
 bc.email2ManageFormr = {
 	init : function(option,readonly) {
 		var $form = $(this);
+		
+		$form.find(".email-open").show();
+		
 		var sendDate=$form.find(":input[name='e.sendDate']").val();
 		var fromNow=moment(sendDate, "YYYY-MM-DD HH:mm:ss").fromNow()
 		$form.find(".emailFormr-fromNow").text(fromNow);
