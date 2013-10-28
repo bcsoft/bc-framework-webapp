eabcf94af6c47bf32cd9505200645a604308c98e
dragon <rongjihuang@gmail.com>
2013-10-28 19:38:17 +0800

    photo

---
+++ photo/photo.js
@@ -0,0 +1,130 @@
+bc.namespace("bc.photo");
+bc.photo = {
+    image: {},
+    /** 初始化 */
+    init: function () {
+        // 浏览器支持检测
+        if (!window.FileReader) {
+            alert("你的浏览器不支持window.FileReader接口，无法处理！");
+            return;
+        }
+
+        bc.photo.image = {
+            type: "png",
+            name: "bc",
+            data: "data:image/png;base64,0"
+        };
+
+        var $form = $(this);
+        var $displayContainer = $form.find("#displayContainer");
+        var $imgDisplayer = $form.find("#imgDisplayer");
+        var $imgProxy = $form.find("#imgProxy");
+        $imgProxy.attr("src", $imgDisplayer.attr("src"));
+
+        // 窗口大小变动后重新处理显示区的缩放
+        $form.on("dialogresize", function (e, ui) {
+            bc.photo.resize.call($form, $displayContainer, $imgDisplayer, $imgProxy);
+        });
+        $form.trigger("dialogresize");
+
+        // 根据图片的实际大小调整显示区，保证图片整张显示
+        $imgProxy.on("load", function (e) {
+            $imgDisplayer.attr("src", $imgProxy.attr("src"));
+            bc.photo.resize.call($form, $displayContainer, $imgDisplayer, $imgProxy);
+        });
+
+        // 底部工具条容器
+        var $tb = $form.closest(".ui-dialog").find(".ui-dialog-buttonpane");
+        // 打开图片事件
+        $tb.find("#openImageBtn>span.ui-button-text").html('<div'
+            + ' style="position:relative;width:100%;height:100%;white-space:nowrap;">打开图片'
+            + '<input type="file" class="uploadFile" name="uploadFile" title="打开图片" multiple="true"'
+            + ' style="position:absolute;left:0;top:0;width:100%;height:100%;filter:alpha(opacity=10);opacity:0;cursor:pointer;"/>'
+            + '</div>');
+        $tb.find(":file.uploadFile").change(function (e) {
+            // 判断是否选中文件：如果曾经选中过文件，再打开对话框选择"取消"按钮，change事件也会执行
+            if (!this.files || this.files.length == 0) {
+                console.log("no file selected");
+                return false;
+            }
+
+            // 判断文件类型：只支持png、jpg格式
+            for (var i = 0; i < this.files.length; i++) {
+                //console.log(this.files[i]);
+                var index = this.files[i].name.lastIndexOf(".");
+                var type = "";
+                if (index != -1) {
+                    type = this.files[i].name.substr(index + 1);
+                }
+                type = type.toLowerCase();
+                if (type != "png" && type != "jpg") {
+                    alert("只能打开 png 或 jpg 格式的图片！");
+                    return false;
+                }
+            }
+
+            // 显示图片：TODO 多选的处理
+            var reader = new window.FileReader();
+            var file = this.files[0];
+            bc.photo.image.type = file.name.substr(file.name.lastIndexOf(".") + 1);
+            bc.photo.image.name = file.name.substring(0, file.name.lastIndexOf("."));
+            reader.onload = function (e) {
+                //console.log(e.target.result);
+                // 将图片数据加载到图片代理控件
+                $imgProxy.attr("src", e.target.result);
+                bc.photo.image.data = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        });
+    },
+
+    /** 重新处理显示区的缩放 */
+    resize: function ($displayContainer, $imgDisplayer, $imgProxy) {
+        //console.log("todo:resize");
+        var iw = $imgProxy[0].width;
+        var ih = $imgProxy[0].height;
+        var iwh = iw / ih;// 图片的实际宽高比
+        var cw = $displayContainer[0].clientWidth;
+        var ch = $displayContainer[0].clientHeight;
+        var cwh = cw / ch;// 显示区的宽高比
+        //console.log(iw + "/" + ih + "=" + iwh + ", " + cw + "/" + ch + "=" + cwh);
+        var sw, sh, padding = 20;
+        if (iwh < cwh) {//  限定高度，宽度自由缩放
+            $imgDisplayer.css({
+                width: "auto",
+                height: ch - padding + "px"
+            });
+        } else {// 限定宽度，高度自由缩放
+            $imgDisplayer.css({
+                width: cw - padding + "px",
+                height: "auto"
+            });
+        }
+    },
+
+    /** 完成 */
+    ok: function () {
+        //alert("todo:ok");
+        var $form = $(this);
+    },
+
+    /** 拍照 */
+    captureCamera: function () {
+        alert("todo:captureCamera");
+    },
+
+    /** 下载 */
+    download: function () {
+        var $form = $(this);
+
+        // 将mime-type改为image/octet-stream，强制让浏览器直接download
+        var imageData = bc.photo.image.data.replace('image/'+bc.photo.image.type,'image/octet-stream');
+        var save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+        save_link.href = imageData;
+        save_link.download = bc.photo.image.name + "." + bc.photo.image.type;
+
+        var event = document.createEvent('MouseEvents');
+        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        save_link.dispatchEvent(event);
+    }
+};
---
+++ photo/photo.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<div title='<s:text name="photo.title"/>' data-type='form' class="bc-page"
+	data-js='<s:url value="/bc/photo/photo.js" />'
+	data-initMethod='bc.photo.init'
+	data-option='<s:property value="pageOption"/>' style="overflow:hidden;">
+<div class="hlayout" style="width: 100%; height: 100%;font-weight:normal">
+    <div id="displayContainer" class="flex vlayout" style="position:relative;overflow:hidden;background-color:#2B2B2B;">
+        <img id="imgDisplayer" style="width:0;" src="bc/photo/asset/A4-手写.jpg"/>
+    </div>
+    <div class="ui-widget-content" style="width: 180px;border-color:#27292A;border-width:0 0 0 1px;background-image:none;background-color:#3C3F41;">TODO</div>
+</div>
+<div style="display:none;"><img id="imgProxy"/></div>
+</div>
