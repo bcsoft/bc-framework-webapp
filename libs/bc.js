18f7cc700b533fdba5d9fb08254b76f3c5f13468
dragon <rongjihuang@gmail.com>
2014-03-05 09:47:18 +0800

    重新合并js

--- libs/bc.js
+++ libs/bc.js
@@ -2035,7 +2035,7 @@ $document.delegate(".bc-button",{
 $document.delegate(".bc-toolbar #searchText","keyup", function(e) {
 	var $this = $(this);
 	if(e.which == 13){//按下回车键
-		var $page = $this.parents(".bc-page");
+		var $page = $this.closest(".bc-page");
 		var $search = $this.parent();
 		bc.toolbar.doSearch($page,{
 			action: $search.attr("data-action"),//内定的操作
@@ -2047,7 +2047,7 @@ $document.delegate(".bc-toolbar #searchText","keyup", function(e) {
 //右侧的搜索框处理：点击左侧的简单搜索按钮
 $document.delegate(".bc-toolbar #searchBtn","click", function(e) {
 	var $this = $(this);
-	var $page = $this.parents(".bc-page");
+	var $page = $this.closest(".bc-page");
 	var $search = $this.parent();
 	bc.toolbar.doSearch($page,{
 		action: $search.attr("data-action"),//内定的操作
@@ -6191,51 +6191,25 @@ $(".bc-imageEditor").live("click",function(e){
  * @depend jquery-ui,bc.core
  */
 (function($) {
-
-bc.browser = {
-	/**下载指定的浏览器安装文件
-	 * @param puid 附件的puid 
-	 */
-	init: function(puid) {
-		var browser = jQuery.browser.version;
-		var lowerVersion = false;
-		var unSupportHtml5 = false;
-		if(browser.safari || browser.mozilla || browser.opera){
-			//对现代化的浏览器不做任何提示
-			//Chrome、Safari、Firefox、Opera
-		}else if(browser.msie){
-			lowerVersion = browser.version < 8;
-			unSupportHtml5 = browser.version < 9;
-		}
-		if(lowerVersion || unSupportHtml5){
-			bc.msg.slide("你的浏览器" + (lowerVersion ? "版本太低" : "") + (unSupportHtml5 ? "、不支持html5！" : "！"));
-			//弹出窗口让用户下载浏览器
-			bc.page.newWin({
-				url: bc.root + "/bc/attach/browser",
-				mid: "attach.browser",
-				name: "下载系统支持的浏览器"
-			});
-		}
-	},
-	/**下载指定的浏览器安装文件
-	 * @param puid 附件的puid 
-	 */
-	download: function(puid) {
-		window.open(bc.root + "/bc/attach/download?puid=" + puid, "blank");
+	var browser = jQuery.browser.version;
+	var lowerVersion = false;
+	var unSupportHtml5 = false;
+	if(browser.safari || browser.mozilla || browser.opera){
+		//对现代化的浏览器不做任何提示
+		//Chrome、Safari、Firefox、Opera
+	}else if(browser.msie){
+		lowerVersion = browser.version < 8;
+		unSupportHtml5 = browser.version < 9;
 	}
-};
-	
-bc.browser.init();
-
-//事件处理
-$("ul.browsers>li.browser").live("mouseover", function() {
-	$(this).addClass("ui-state-hover");
-}).live("mouseout", function() {
-	$(this).removeClass("ui-state-hover");
-}).live("click", function() {
-	bc.browser.download($(this).attr("data-puid"));
-});
-
+	if(lowerVersion || unSupportHtml5){
+		bc.msg.slide("你的浏览器" + (lowerVersion ? "版本太低" : "") + (unSupportHtml5 ? "、不支持html5！" : "！"));
+		//弹出窗口让用户下载浏览器
+		bc.page.newWin({
+			url: bc.root + "/bc/browser/list",
+			mid: "browser.list",
+			name: "下载系统支持的浏览器"
+		});
+	}
 })(jQuery);
 /**
  * 自动处理溢出的tabs
