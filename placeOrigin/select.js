7ff4cdad8bfa4512ec0766cec9208b601d7622e5
dragon <rongjihuang@gmail.com>
2014-07-09 16:32:12 +0800

    增加fullname

--- placeOrigin/select.js
+++ placeOrigin/select.js
@@ -21,18 +21,23 @@ bc.superiorPlaceSelectDialog = {
 			data.code= $tr.find("td:eq(1)").text();
 			data.name = $tr.find("td:eq(2)").text();
 			data.pname= $tr.find("td:eq(3)").text();
+			data.fullname= (data.pname && data.pname.length > 0) ? data.pname + "/" + data.name : data.name;
 		}else{
 			data=[];
 			var $right = $grid.find(">.data>.right");;
+            var pname,name;
 			$tds.each(function(i){
 				var $this = $(this);
 				var $row = $right.find("tr.row:eq(" + $this.parent().index() + ")");
+                name =  $row.find("td:eq(2)").text();
+                pname =  $row.find("td:eq(3)").text();
 				data.push({
 					id: $this.attr("data-id"),
 					type: $row.find("td:eq(0)").text(),
 					code: $row.find("td:eq(1)").text(),
-					name: $row.find("td:eq(2)").text(),
-					pname: $row.find("td:eq(3)").text()
+					name: name,
+					pname:pname,
+                    fullname: (pname && pname.length > 0) ? pname + "/" + name : name
 				});
 			});	
 		}
