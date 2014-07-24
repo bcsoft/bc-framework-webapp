0d6f6106cd6f2a2f0b2837eb0a35461aefd26f2d
Action <572333932@qq.com>
2014-07-24 18:11:38 +0800

    优化选择角色对话框_前台

--- identity/role/select.js
+++ identity/role/select.js
@@ -1,31 +1,41 @@
-bc.selectRole = {
-	init : function() {
-		var $page = $(this);
-		//绑定双击事件
-		$page.find("select").dblclick(function(){
-			bc.selectRole.clickOk.call($page[0]);
-		});
-	},
-	clickOk : function() {
-		var $page = $(this);
-		var select = $page.find("select")[0];
-		if(select.selectedIndex == -1){
-			bc.msg.slide("必须先选择角色信息！");
-			return false;
-		}
-		var item;
-		if(select.multiple){//多选
-			item=[];
-			// 循环选定的每一个项目，将该项添加到列表中
-			for (var i = 0; i < select.length; i++){
-				if (select.options[i].selected){
-					item.push({id: select.options[i].value,name: select.options[i].text});
-				}
-			}
-		}else{//单选
-			item={id: select.value,name: select.options[select.selectedIndex].text};
-		}
-		$page.data("data-status",item);
-		$page.dialog("close");
-	}
-}
+if(!window['bc'])window['bc']={};
+bc.roleSelectDialog = {
+    /** 点击确认按钮后的处理函数 */
+    clickOk : function() {
+        var $page = $(this);
+        
+        // 获取选中的行的id单元格
+        var $tds = $page.find(".bc-grid>.data>.left tr.ui-state-highlight>td.id");
+        if($tds.length == 0){
+            alert("请先选择！");
+            return false;
+        }
+
+        // 获取选中的数据
+        var data;
+        var $grid = $page.find(".bc-grid");
+        if($grid.hasClass("singleSelect")){//单选
+            data = {};
+            data.id = $tds.attr("data-id");
+            var $trs = $grid.find(">.data>.right tr.ui-state-highlight");
+            data.name = $trs.find("td:eq(1)").attr("data-value");
+            data.account = $trs.find("td:eq(2)").attr("data-value");
+            $.extend(data,$trs.data("hidden"));
+        }else{//多选
+            data = [];
+            var $trs = $grid.find(">.data>.right tr.ui-state-highlight");
+            $tds.each(function(i){
+                data.push($.extend({
+                    id: $(this).attr("data-id"),
+                    name:$($trs.get(i)).find("td:eq(1)").attr("data-value"),
+                    account:$($trs.get(i)).find("td:eq(2)").attr("data-value")
+                },$($trs.get(i)).data("hidden")));
+            });
+        }
+        logger.info($.toJSON(data));
+        
+        // 返回
+        $page.data("data-status", data);
+        $page.dialog("close");
+    }
+};
