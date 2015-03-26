3f7866dae31be4c33646015711a453ebe86c0f3e
dragon <rongjihuang@gmail.com>
2015-03-26 14:06:28 +0800

    将待办边栏改为使用AngularJS实现 - 比Backbone简单多了

--- index/sidebar/sidebar.js
+++ index/sidebar/sidebar.js
@@ -1,425 +1,127 @@
-bc.namespace("bc.sidebar");
-(function($){
+var sidebarApp = angular.module('sidebarApp', []);
 
-// 通知：id,type,title,content,time
-var Notice = Backbone.Model.extend({
-	// 初始化
-	initialize: function() {
-	 	if (!this.get("iconClass")) {
-	 		var parentIconClass = GroupTypes[this.get("type")].itemIconClass;
-	 		if(parentIconClass)
-		 		this.set({"iconClass": parentIconClass});
-	 		else
-		 		this.set({"iconClass": "ui-icon-radio-on"});
-		}
-	 	if (!this.get("time4moment")) {
-	 		this.set({"time4moment": moment(this.get("time"), "YYYY-MM-DD HH:mm:ss").fromNow()});
-		}
-	}
-});
+sidebarApp.controller('SidebarCtrl', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+	$scope.orderProp = '-time';
+	$scope.query = '';
+	$scope.groups = [
+		{	mid: "myEmails",
+			type: "email",
+			title: "邮件",
+			url: "bc/emailTos/paging",
+			itemUrl: "bc/email/open",
+			itemData: "openType=2&id={0}",
+			iconClass: "ui-icon-mail-closed",
+			itemIconClass: "ui-icon-mail-closed",
+			count: 0,
+			order: 1},
+		{	mid: "workspace",
+			type: "todo",
+			title: "个人待办",
+			url: "bc-workflow/todo/personals/list",			// 视图url
+			itemUrl: "bc-workflow/workspace/open",	// 表单url
+			itemData: "id={0}",
+			iconClass: "ui-icon-calendar",
+			itemIconClass: "ui-icon-check",
+			count: 0,
+			order: 2},
+		{	mid: "workspace",
+			type: "groupTodo",
+			title: "岗位待办",
+			url: "bc-workflow/todo/personals/list",
+			itemUrl: "bc-workflow/workspace/open",
+			itemData: "id={0}",
+			iconClass: "ui-icon-calendar",
+			itemIconClass: "ui-icon-person",
+			count: 0,
+			order: 3}
+	];
+	$scope.typeIndex = {};
+	angular.forEach($scope.groups, function(group, index) {
+		$scope.typeIndex[group.type] = index;
+	});
+	console.log("sidebar: typeIndex", $scope.typeIndex);
 
-// 通知列表
-var NoticeList = Backbone.Collection.extend({
-	// 获取数据的URl
-	url: function(){
-		return "sidebar";
-	},
-
-	// 关联到通知
-	model: Notice,
-	
-	comparator: "time",
-	
-	// 初始化
-	initialize: function() {
-	}
-});
-
-// 创建一个通知列表实例
-var notices = new NoticeList;
-
-// 通知条目的视图
-var NoticeView = Backbone.View.extend({
-	tagName: "div",
-	className: "row",
-
-	// Cache the template function for a single item.
-	template: _.template('<div class="header row">'
-		+'		<div class="icon ui-icon <%- iconClass %>"></div>'
-		+'		<div class="label" title="<%- title %>"><%- title %></div>'
-		+'		<div class="time"><%- time4moment %></div>'
-		+'	</div>'
-		+'	<pre class="detail" title="<%- content %>"><%- content %></pre>'),
-
-	// 绑定DOM事件
-	events: {
-		"click": "open"
-	},
-
-	// 初始化
-	initialize: function() {
-		// 监听模块的事件
-		//this.listenTo(this.model, 'change', this.render);
-		this.listenTo(this.model, 'change:time4moment', this.updateTime4moment);
-		this.listenTo(this.model, 'destroy', this.remove);
-	},
-
-	// 渲染
-	render: function() {
-		if(logger.infoEnabled)logger.info("m:render");
-		this.$el.html(this.template(this.model.toJSON()));
-		this.time = this.$(">.header>.time");
-		return this;
-	},
-
-	// 自动更新相对时间的显示
-	updateTime4moment: function() {
-		if(logger.debugEnabled)logger.debug("m:updateTime4moment");
-		this.time.html(this.model.get("time4moment"));
-		return this;
-	},
-
-	// 打开链接
-	open: function() {
-		var g = GroupTypes[this.model.get("type")];
-		
-		//邮件
-		if(this.model.get("type")=='email'){
+	// 打开单条信息
+	$scope.openItem = function(item){
+		console.log("openItem", item);
+		var g = $scope.groups[$scope.typeIndex[item.type]];
+		if(item.type == 'email'){	//邮件
 			bc.page.newWin({
-				name: this.model.get("title") || "(无)",
-				mid: this.model.get("type") + this.model.get("dbid"),
+				name: item.title || "(无)",
+				mid: item.type + item.dbid,
 				url: g.itemUrl,
-				data: g.itemData.format(this.model.get("dbid"))
+				data: g.itemData.format(item.dbid)
 			});
-			
-		}else{
+		}else{						// 待办
 			bc.flow.openWorkspace({
-				name : this.model.get("title") || "(无)",
-				id : this.model.get("dbid")
+				name : item.title || "(无)",
+				id : item.dbid
 			});
-		}		
-	}
-});
+		}
+	};
 
-//通知条目的视图
-var NoticeGroupView = Backbone.View.extend({
-	tagName: "div",
-	className: "group",
-	template: _.template(
-		'<div class="header row ui-widget-header">'
-			+'<div class="icon u i-icon <%- iconClass %>"></div>'
-			+'<div class="label"><span class="title"><%- title %></span> (<span class="count">0</span>)</div>'
-			+'<div class="btn toggle ui-icon ui-icon-triangle-1-n" title="点击折叠"></div>'
-		+'</div>'
-		+'<div class="rows"><div class="empty">(无)</div></div>'),
-	empty: '<div class="empty">(无)</div>',
-
-	// 绑定DOM事件
-	events: {
-		"click .header>.label>span": "open",
-		"click .header>.btn.toggle": "toggle"
-	},
-
-	// 初始化
-	initialize: function() {
-		this.count = 0;
-		this.itemViews = {};
-		// 初始化dom结构
-		this.render();
-	},
-
-	// 渲染
-	render: function() {
-		if(logger.infoEnabled)logger.info("mg:render");
-		this.$el.html(this.template(this.model));
-		this.rows = this.$(">.rows");
-		this.toggleBtn = this.$(">.header>.btn.toggle");
-		this.countEl = this.$(">.header>.label>.count");
-		return this;
-	},
-
-	// 打开更多视图
-	open: function() {
-		var g = this.model;
+	// 打开各区域的视图
+	$scope.openGroup = function(group){
+		console.log("openGroup", group);
 		bc.page.newWin({
-			name: g.title,
-			mid: g.mid,
-			url: g.url
+			name: group.title,
+			mid: group.mid,
+			url: group.url
 		});
-	},
+	};
 
-	// 关闭分组
-	close: function() {
-		alert("TODO: group.close");
-	},
+	// 折叠|展开所有区域
+	$scope.toggleAll = function(){
+		angular.forEach($scope.groups, function(group, index) {
+			group.collapse = !group.collapse;
+		});
+	};
 
-	// 在组中添加一项
-	add: function(notice) {
-		if(this.rows.is(":has(.empty)")){
-			this.rows.empty();
-		}
-	    var view = new NoticeView({model: notice});
-	    this.rows.append(view.render().el);
-	    this.itemViews[notice.cid] = view;
-	    this.count++;
-	    this.countEl.text(this.count);
-	},
+	// 重新加载数据
+	$scope.refresh = function(){
+		console.log("sidebar: refresh", new Date());
+		$scope.refreshing = true;
+		$http.get('/sidebar').success(function(data) {
+			$scope.items = data;
+			$scope.refreshing = false;
 
-	// 在组中移除一项
-	removeItem: function(notice) {
-		if(logger.infoEnabled)logger.info("group:removeItem");
-	    this.itemViews[notice.cid].remove();
-	    delete this.itemViews[notice.cid];
-		if(this.rows.is(":empty"))
-			this.rows.append(this.empty);
-	    this.count--;
-	    this.countEl.text(this.count);
-	},
+			// 更新相对时间
+			$scope.time4moment();
 
- 	// 搜索
- 	doSearch: function(text) {
-		if(logger.infoEnabled)logger.info("mg:doSearch:" + this.$el.find(">.header>.label>span").text() + ":" + text);
- 	   	
-		var view,model;
-		for(var key in this.itemViews){
-			view = this.itemViews[key];
-			model = view.model;
-			//if(logger.infoEnabled)logger.info("id=" + model.id + "," + model.get("title"));
-			if(model.get("title").indexOf(text) != -1 
-				|| (model.has("content") && model.get("content").indexOf(text) != -1)){
-				if(logger.infoEnabled)logger.info("match:title=" + model.get("title"));
-				view.$el.show();
-			}else{
-				view.$el.hide();
-			}
-		}
- 	   	return false;
- 	},
-
-	// 折叠或展开
-	toggle: function() {
-		this.$el.toggleClass("collapse");
-		var isCollapse = this.$el.hasClass("collapse");
-		if(isCollapse){
-			this.toggleBtn.toggleClass("ui-icon-triangle-1-s",true)
-				.toggleClass("ui-icon-triangle-1-n",false)
-				.attr("title","点击展开");
-		}else{
-			this.toggleBtn.toggleClass("ui-icon-triangle-1-n",true)
-				.toggleClass("ui-icon-triangle-1-s",false)
-				.attr("title","点击折叠");
-		}
-		return false;
-	}
-});
-
-// 预定义的分组配置
-var GroupTypes={
-	email: {
-		mid: "myEmails",
-		title: "邮件",
-		url: "bc/emailTos/paging",
-		itemUrl: "bc/email/open",
-		itemData: "openType=2&id={0}",
-		iconClass: "ui-icon-mail-closed",
-		itemIconClass: "ui-icon-mail-closed",
-		order: 3
-	},
-	todo: {
-		mid: "workspace",
-		title: "个人待办",
-		url: "bc-workflow/todo/personals/list",			// 视图url
-		itemUrl: "bc-workflow/workspace/open",	// 表单url
-		itemData: "id={0}",
-		iconClass: "ui-icon-calendar",
-		itemIconClass: "ui-icon-check",
-		order: 1
-	},
-	groupTodo: {
-		mid: "workspace",
-		title: "岗位待办",
-		url: "bc-workflow/todo/personals/list",
-		itemUrl: "bc-workflow/workspace/open",
-		itemData: "id={0}",
-		iconClass: "ui-icon-calendar",
-		itemIconClass: "ui-icon-person",
-		order: 2
-	}
-};
-
-// 待办边栏视图
-var SidebarView = Backbone.View.extend({
-	el: $("#right"),
-	groupViews: {},
-
-	// 界面模版
-	template: _.template(
-		 '<div class="header ui-widget-header">'
-			+'<div class="btns">'
-//				+'<div class="btn toggle">'
-//					+'<span class="ui-icon ui-icon-transferthick-e-w"></span>'
-//				+'</div>'
-				+'<div class="btn refresh" title="刷新">'
-					+'<span class="ui-icon ui-icon-refresh"></span>'
-				+'</div>'
-				+'<div class="btn toggle" title="展开|折叠信息">'
-					+'<span class="ui-icon ui-icon-carat-2-n-s"></span>'
-				+'</div>'
-			+'</div>'
-			+'<div class="label">通知中心</div>'
-			+'<div class="search">'
-				+'<a href="#" class="ui-icon ui-icon-search" title="点击执行查询"></a>'
-				+'<input type="text" class="ui-widget-content" placeholder="搜索">'
-			+'</div>'
-		+'</div>'
-		+'<div class="groups ui-widget-content"></div>'),
-
-	// 定义事件委托
-	events: {
-		"click .header>.btns>.btn.refresh": "refresh",
-		"click .header>.btns>.btn.toggle": "toggle",
-		"keypress .header>.search>input": "doSearchOnEnter",
-		"click .header>.search>a": "doSearch"
-	},
-
-	// 初始化
-	initialize: function() {
-		var _this = this;
-		// 初始化dom结构
-		this.render();
-		
-		// 监听模块列表的事件
-		this.listenTo(notices, 'add', this.add);
-		this.listenTo(notices, 'reset', this.reset);
-		this.listenTo(notices, 'remove', this.removeItem);
-		
-		// 每间隔一段时间更新相对时间的值
-		window.setInterval(function(){
-			if(_this.refreshing) return;
-			logger.info("sidebar:updateMoment");
-			notices.each(function(m){
-				m.set({"time4moment": moment(m.get("time"), "YYYY-MM-DD HH:mm:ss").fromNow()});
+			// 汇总各分组的数量
+			angular.forEach($scope.groups, function(group) {
+				group.count = 0;
 			});
-		},60000);
-		
-		this.refresh();
-		// 每间隔一段时间重新获取数据
-		window.setInterval(function(){
-			_this.refresh();
-		},300000);
-	},
-
- 	// 渲染
- 	render: function() {
-		if(logger.infoEnabled)logger.info("sidebar:render");
-		this.$el.toggleClass("sidebar",true).html(this.template({}));
-		
-		// 缓存一些内部的dom对象
-		this.input = this.$(">.header>.search>input");
-		this.groups = this.$(">.groups");
-		
-		// 生成空的分组
-		for(var key in GroupTypes){
-		    var groupView = new NoticeGroupView({
-		    	model: GroupTypes[key]
-		    });
-		    this.groupViews[key] = groupView;
-		    this.groups.append(groupView.el);
-		}
-
-		return this;
-	},
-
- 	// 配置通知中心
- 	config: function () {
- 		alert("TODO: config");
- 	},
-
- 	// 回车后搜索
- 	doSearchOnEnter: function(e) {
- 	   	if (e.keyCode != 13) return;
- 	   	this.doSearch();
- 	   	return false;
- 	},
-
- 	// 搜索
- 	doSearch: function() {
-		if(logger.infoEnabled)logger.info("sidebar:doSearch:" + this.input.val());
- 	   	
-		for(var key in this.groupViews){
-			this.groupViews[key].doSearch(this.input.val());
-		}
- 	   	return false;
- 	},
-
-	// 在组中添加一项
-	add: function(notice) {
-		if(logger.infoEnabled)logger.info("sidebar:add");
-		var type = notice.get("type");
-		if(!this.groupViews[type]){
-			// 创建分组
-		    var groupView = new NoticeGroupView({
-		    	model: GroupTypes[type]
-		    });
-		    this.groupViews[type] = groupView;
-		    this.groups.append(groupView.el);
-		}
-		
-		// 添加信息条目
-		this.groupViews[type].add(notice);
-	},
-	
-	// 在组中移除1项
-	removeItem: function(model) {
-		if(logger.infoEnabled)logger.info("sidebar:removeItem=" + model.get("type"));
-		this.groupViews[model.get("type")].removeItem(model);
-	},
-
-	// 在组中重新添加全部
-	reset: function(models) {
-		if(logger.infoEnabled)logger.info("sidebar:reset");
-		this.groups.empty();
-		this.groupViews = {};
-	},
-
-	// 刷新
-	refresh: function(callback) {
-		var _this = this;
-		logger.info("sidebar:refreshing=" + this.refreshing);
-		//this.input.val("");
-		if(this.refreshing) return;
-		this.refreshing = true;
-		
-		// 清空数据
-		notices.each(function(model){
-			notices.remove(model);
+			var type;
+			angular.forEach($scope.items, function(item) {
+				type = $scope.groups[$scope.typeIndex[item.type]];
+				type && (type.count += 1);
+			});
 		});
+	};
 
-		// 加载数据
-		logger.info("sidebar:refresh-start");
-		notices.fetch({
-			reset: false,
-			success:function(){
-				logger.info("sidebar:refresh-success");
-				_this.refreshing = false;
-				_this.doSearch();
-			},
-			error: function(){
-				logger.info("sidebar:refresh-error");
-				_this.refreshing = false;
-			}
+	// 更新相对时间
+	$scope.time4moment = function(){
+		console.log("sidebar: update time4moment", new Date());
+		angular.forEach($scope.items, function(item) {
+			item.time4moment = moment(item.time, "YYYY-MM-DD HH:mm:ss").fromNow();
 		});
-	},
+	};
 
-	// 折叠或展开
-	toggle: function() {
-		for(var key in this.groupViews){
-			this.groupViews[key].toggle();
-		}
-		return false;
-	}
-});
+	// 每间隔一段时间更新相对时间
+	$interval(function() {
+		if($scope.refreshing) return;
+		$scope.time4moment(); 
+	}, 60000);
 
-// 创建边栏的实例
-bc.sidebar = new SidebarView;
-})(jQuery);
+	// 每间隔一段时间重新获取数据
+	$interval(function(){
+		$scope.refresh();
+	}, 300000);
+
+
+	// 初始化一次数据
+	$scope.refresh();
+}]);
+
+//angular.bootstrap(document, ['sidebarApp']);
